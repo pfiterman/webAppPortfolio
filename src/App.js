8df5cae88a85e4441aa2486f9f4ed17c66c393b7
Pablo Fiterman <pfiterman@gmail.com>
2025-01-27 22:52:43 -0500

    Remove ReactQuery Devtools

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
