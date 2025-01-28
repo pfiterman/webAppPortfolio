113b1101166aaef000319c73312fb00657e73ce9
Pablo Fiterman <pfiterman@gmail.com>
2025-01-27 23:28:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
